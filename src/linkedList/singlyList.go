33c969fb145198de508b50266a187e5f6bf56ef3
vladpereskokov <vladpereskokov@mail.ru>
2017-09-18 19:36:38 +0300

    add new list func

--- src/linkedList/singlyList.go
+++ src/linkedList/singlyList.go
@@ -22,3 +22,18 @@ type SinglyList struct {
 	len  int
 	head *SinglyListItem
 }
+
+func (list SinglyList) Len() int {
+	return list.len
+}
+
+func (list SinglyList) Head() *SinglyListItem {
+	return list.head
+}
+
+func New() *SinglyList {
+	list := new(SinglyList)
+	list.len = 0
+
+	return list
+}
