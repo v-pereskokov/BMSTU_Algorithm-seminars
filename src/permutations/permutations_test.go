8804c15ad86775aea8f154174dfba4a45c58dde3
vladpereskokov <vladpereskokov@mail.ru>
2017-09-18 17:38:51 +0300

    make test perm

--- src/permutations/permutations_test.go
+++ src/permutations/permutations_test.go
@@ -3,24 +3,22 @@ package permutations
 import "testing"
 
 func TestPermutations(t *testing.T) {
-	checkArray := []int{
-		1, 2, 3,
-		1, 3, 2,
-		2, 1, 3,
-		2, 3, 1,
-		3, 1, 2,
-		3, 2, 1,
+	checkArray := []string{
+		"[1 2 3]",
+		"[1 3 2]",
+		"[2 1 3]",
+		"[2 3 1]",
+		"[3 1 2]",
+		"[3 2 1]",
 	}
 	checkIndex := 0
 
 	for permutation := range Permutations(3) {
 		t.Log(permutation)
-		for _, value := range permutation {
-			if value != checkArray[checkIndex] {
-				t.Errorf("Don't match: %d and %d \n", value, checkArray[checkIndex])
-			}
-
-			checkIndex++
+		if string(permutation) != checkArray[checkIndex] {
+			t.Errorf("Don't match: %s and %s \n", string(permutation), checkArray[checkIndex])
 		}
+
+		checkIndex++
 	}
 }
