8cd0ea5d5593f995e349086816712ee906e6d17f
vladpereskokov <vladpereskokov@mail.ru>
2017-09-18 15:31:51 +0300

    change permutations

--- src/permutations/permutations.go
+++ src/permutations/permutations.go
@@ -1,11 +1,38 @@
 package permutations
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/BMSTU_Algorithm-seminars/src/utils"
+)
 
 func Permutations(number int) {
-	for i := range make([]struct{}, number) {
+	list := utils.Range(number)
+
+	for i := range list {
 		fmt.Printf("%d ", i)
 	}
 
-	fmt.Println("")
+	for {
+		i := number - 2
+
+		for i >= 0 && list[i] >= list[i+1] {
+			i--
+		}
+
+		if i < 0 {
+			return
+		}
+
+		min := i + 1
+
+		for j := i + 2; ; j++ {
+			if list[j] > list[i] && list[j] < list[min] {
+				min = j
+			}
+		}
+		
+		list[i], list[min] = list[min], list[i]
+		
+		
+	}
 }
